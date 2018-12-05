f27a5ba86ac662c2e1798c8a137fec12adc8daad
patapum123 <vinogradskiy.aleksandr@gmail.com>
2018-12-05 16:42:29 +0100

    first

---
+++ master/style-switcher/style.switcher.localstorage.js
@@ -0,0 +1,24 @@
+/*
+Name: 			Style Switcher Initializer
+Written by: 	Okler Themes - (http://www.okler.net)
+Version: 		2.0
+*/
+
+if (typeof localStorage !== "undefined") {
+	if (localStorage.getItem('skin.css') !== null && !document.querySelector('html').hasAttribute('data-style-switcher-options')) {
+
+		var css = localStorage.getItem('skin.css'),
+		    head = document.head || document.getElementsByTagName('head')[0],
+		    style = document.createElement('style');
+
+		style.type = 'text/css';
+		if (style.styleSheet) {
+		    style.styleSheet.cssText = css;
+		} else {
+		    style.appendChild(document.createTextNode(css));
+		}
+
+		head.appendChild(style);
+
+	}
+}
